72cc9c3b855dc16a1d60021fbb1ad2872e7af129
Graham Howden <ghowden@and.digital>
2017-10-31 14:30:13 +0000

    makes user-details a card style

--- src/components/user-details/index.js
+++ src/components/user-details/index.js
@@ -15,7 +15,7 @@ export default class UserDetails extends React.PureComponent {
 
   render() {
     return (
-      <div>
+      <div className="pt-card">
         <h1 className="user-full-name">{this.props.user.forename} {this.props.user.surname}</h1>
         <div className="user-nationality">{this.props.user.nationality}</div>
         <div className="user-age"><YearsFromNow time={this.props.user.dob}/> years old</div>
