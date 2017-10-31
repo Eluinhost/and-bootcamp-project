5302f9c046d73ec427e3819ef16cbe4ced04ef41
Armin <arminpudic@yahoo.co.uk>
2017-10-31 15:37:39 +0000

    adds another user to temporary rendering test

--- src/App.js
+++ src/App.js
@@ -3,18 +3,26 @@ import './App.css';
 import UserList from './components/user-list';
 import moment from 'moment';
 
-const user = {
-  forename: 'Peter',
-  surname: 'Swift',
-  nationality: 'British',
-  dob: moment().subtract(22, 'year').subtract(4, 'month').subtract(21, 'day'),
-};
+const users = [
+  {
+    forename: 'Peter',
+    surname: 'Swift',
+    nationality: 'British',
+    dob: moment().subtract(22, 'year').subtract(4, 'month').subtract(21, 'day'),
+  },
+  {
+    forename: 'Phil',
+    surname: 'Potts',
+    nationality: 'British',
+    dob: moment().subtract(32, 'year'),
+  },
+];
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <UserList users={[user]}/>
+        <UserList users={users}/>
       </div>
     );
   }
