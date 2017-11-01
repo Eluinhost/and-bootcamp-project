dfa546ee804a2291598b9d69b79756dec7c17282
peterswift <swiftyp106@googlemail.com>
2017-11-01 10:56:24 +0000

    Adds list dialogs with user details.

--- src/components/user-list-row/index.js
+++ src/components/user-list-row/index.js
@@ -2,21 +2,33 @@ import * as React from 'react';
 import UserDetailsPropTypes from '../../prop-types/UserDetailsPropTypes';
 import PropTypes from 'prop-types'
 import UserDetails from '../user-details';
-import { NonIdealState } from '@blueprintjs/core';
+import YearsFromNow from '../years-from-now';
+import { Dialog } from '@blueprintjs/core';
 
 
 export default class UserListRow extends React.PureComponent {
+  state = {
+    isOpen: false,
+  };
+
   static propTypes = {
     user: UserDetailsPropTypes.isRequired,
   };
 
-  render() {
+  onClick = () => this.setState({ isOpen: true });
+  onDialogClose = () => this.setState({ isOpen: false });
 
+  render() {
     const fullName = this.props.user.forename + ' ' + this.props.user.surname;
 
     return (
-      <div className = "user-list-row">
-        {fullName}
+      <div className="user-list-row" onClick={this.onClick}>
+        <span className="user-list-row--name">{fullName}</span>
+        <Dialog isOpen={this.state.isOpen} title="User Details" onClose={this.onDialogClose}>
+          <div className="pt-dialog-body">
+            <UserDetails user={this.props.user}/>
+          </div>
+        </Dialog>
       </div>
     )
   }
--- src/components/user-list-row/index.spec.js
+++ src/components/user-list-row/index.spec.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import UserListRow from '.';
 import moment from 'moment';
+import { Dialog } from '@blueprintjs/core';
 
 const user =
 {
@@ -16,5 +17,40 @@ test('UserListRow renders forename and surname "Peter Swift"', () => {
     <UserListRow user={user} />
   );
 
-  expect(component.find('.user-list-row').text()).toEqual('Peter Swift');
+  expect(component.find('.user-list-row--name').text()).toEqual('Peter Swift');
+});
+
+test('UserListRow dialog closed on default', () => {
+  const component = shallow(
+    <UserListRow user={user} />
+  );
+
+  expect(component.state().isOpen).toEqual(false);
+  expect(component.find(Dialog).length).toEqual(1);
+  expect(component.find(Dialog).props().isOpen).toEqual(false);
+});
+
+test('UserListRow dialog opens on click', () => {
+  const component = shallow(
+    <UserListRow user={user} />
+  );
+
+  component.simulate('click');
+
+  expect(component.state().isOpen).toEqual(true);
+  expect(component.find(Dialog).length).toEqual(1);
+  expect(component.find(Dialog).props().isOpen).toEqual(true);
+});
+
+test('UserListRow dialog closes on close', () => {
+  const component = shallow(
+    <UserListRow user={user} />
+  );
+
+  component.instance().onDialogClose();
+
+  expect(component.state().isOpen).toEqual(false);
+  expect(component.find(Dialog).length).toEqual(1);
+  expect(component.find(Dialog).props().isOpen).toEqual(false);
+  expect(component.find(Dialog).props().onClose).toEqual(component.instance().onDialogClose);
 });
